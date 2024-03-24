892a8e198f5ff7f7f49a40f66e48b5f2a6a96a98
Sidharth <sidharthpunalur@gmail.com>
2024-03-24 12:44:08 +0530

    Added todo adding list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React,{useState} from 'react';
+import React from 'react';
 import UI from './UI';
 import './App.css';
 
 
 function App() {
- const [state,setState]=useState(false);
-
-const check=()=>{
-  setState(!state);
-}
-
+ 
   return (
     <div>
-       {state && <UI/>}
-      
-      <h1 onClick={check}>I am a button</h1>
+      <UI/>
     </div>
   )
 }
--- src/UI.jsx
+++ src/UI.jsx
@@ -1,12 +1,42 @@
-import React ,{useEffect} from 'react'
+import React,{useState} from 'react'
 
 function UI() {
-         useEffect(()=>{
-          console.log("mounting")
-         })
+        let day=1;
+        const [toDos,setToDos]=useState([]);
+        const [toDo,setToDo]=useState('');
   return (
-    <div>
-           <h1>I am a counter </h1>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Day {day} </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder=" Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id: Date.now() ,text:toDo,status:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        { 
+
+              toDos.map((list)=>{
+
+             
+        return(
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>{list.text}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+        )
+         })
+        }
+      </div>
     </div>
   )
 }
